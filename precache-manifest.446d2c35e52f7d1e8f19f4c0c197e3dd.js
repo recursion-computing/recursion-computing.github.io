72b18c3b49e596933452b899ea1b79043db56823
nickmaleki <xnick99@gmail.com>
2021-08-29 03:16:22 -0400

    Updates

---
+++ precache-manifest.446d2c35e52f7d1e8f19f4c0c197e3dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8984395bf50dc310ba4c7e0d14712dd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "152c522176c1b399b64d",
+    "url": "/static/css/main.eadf094a.chunk.css"
+  },
+  {
+    "revision": "d50b9766b51fe3bfdbfc",
+    "url": "/static/js/2.bc801c77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc801c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "152c522176c1b399b64d",
+    "url": "/static/js/main.3a3774e2.chunk.js"
+  },
+  {
+    "revision": "051262bd1f7ffaec06c7",
+    "url": "/static/js/runtime-main.e7b42cd2.js"
+  }
+]);
